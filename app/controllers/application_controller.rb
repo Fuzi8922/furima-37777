93071a1971262cdfcdd2e2732395fda265a9a75d
Fuzi8922 <voyage1969to70@gmail.com>
2022-04-08 19:02:43 +0900

    メールアドレスとパスワードのパラメーターを受け取る記述を削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [
-                                        :nickname, :email, :encrypted_password, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday
+                                        :nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday
                                       ])
   end
 
