c0828cb675fe44c972a1ff3b7a367fa021118fe3
Fuzi8922 <voyage1969to70@gmail.com>
2022-04-09 17:10:22 +0900

    Itemモデルのバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,4 +10,27 @@ class Item < ApplicationRecord
   belongs_to :estimated_shipping
   belongs_to :shipping_cost
 
+
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :explanation
+    validates :price
+    validates :category_id
+    validates :condition_id
+    validates :shipping_cost_id
+    validates :prefecture_id
+    validates :estimated_shipping_id
+  end
+
+  with_options format: { with: /\A[0-9]+\z/ },numericality: { other_than: 1 ,message: "can't be blank"} do
+    validates :category_id
+    validates :condition_id
+    validates :shipping_cost_id
+    validates :prefecture_id
+    validates :estimated_shipping_id
+  end
+
+  validates :price, format: { with: /\A[0-9]+\z/ }, numericality: {in: 300..9999999, only_integer: true}
+
 end
