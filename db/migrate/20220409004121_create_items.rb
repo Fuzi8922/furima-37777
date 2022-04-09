3b88f2131f768f9c7c3abc0670a1db40cd0d1cf4
Fuzi8922 <voyage1969to70@gmail.com>
2022-04-09 10:59:35 +0900

    itemsテーブルのマイグレーション定義

--- db/migrate/20220409004121_create_items.rb
+++ db/migrate/20220409004121_create_items.rb
@@ -1,6 +1,15 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
+      t.string :name,                   null: false
+      t.text :explanation,              null: false
+      t.integer :price,                 null: false
+      t.integer :category_id,           null: false
+      t.integer :condition_id,          null: false
+      t.integer :prefecture_id,         null: false
+      t.integer :estimated_shipping_id, null: false
+      t.references :user,               null: false, foreign_key: true
+      t.integer :shipping_cost_id,      null: false
 
       t.timestamps
     end
