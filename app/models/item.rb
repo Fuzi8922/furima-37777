780101beea93b097884768a7df195f93197bf31c
Fuzi8922 <voyage1969to70@gmail.com>
2022-04-09 11:42:05 +0900

    activehashを用いたセレクトボックスの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,11 @@ class Item < ApplicationRecord
 
   belongs_to :user
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :prefecture
+  belongs_to :estimated_shipping
+  belongs_to :shipping_cost
+
 end
