355302d11655858919544e856ad42ca077be18d2
Fuzi8922 <voyage1969to70@gmail.com>
2022-04-09 20:11:33 +0900

    Itemモデルの単体テストコードの実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :item do
-    
+    name                  { Faker::Name.name }
+    explanation           {"explanation-text"}
+    price                 {1000}
+    category_id           {2}
+    condition_id          {2}
+    shipping_cost_id      {2}
+    prefecture_id         {2}
+    estimated_shipping_id {2}
   end
 end
