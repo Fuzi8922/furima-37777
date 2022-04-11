d4e54b27a20dc51ea146894e81cb029005b2544b
Fuzi8922 <voyage1969to70@gmail.com>
2022-04-11 14:39:22 +0900

    editアクション、updateアクション内の処理を修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :edit]
 
   def index
     @items = Item.all.order('created_at desc')
@@ -23,11 +23,20 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id])
+    if current_user.id != @item.user_id
+      redirect_to action: :index
+    end
   end
 
   def update
-    item = Item.find(params[:id])
-    item.update(item_params)
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      @item.update(item_params)
+      redirect_to action: :show
+    else
+      render :edit
+    end
   end
 
   private
