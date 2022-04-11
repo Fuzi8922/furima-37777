998a534c2bb795f7c875ae1fa3e4e33329006eaf
Fuzi8922 <voyage1969to70@gmail.com>
2022-04-11 15:09:28 +0900

    updateアクションの不要な記述を削除、複数のアクションに共通する処理の取り纏め

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
+  before_action :identify_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.all.order('created_at desc')
@@ -19,18 +20,14 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
     redirect_to action: :index if current_user.id != @item.user_id
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
-      @item.update(item_params)
       redirect_to action: :show
     else
       render :edit
@@ -45,5 +42,9 @@ class ItemsController < ApplicationController
     ).merge(
       user_id: current_user.id
     )
+  end
+
+  def identify_item
+    @item = Item.find(params[:id])
   end
 end
