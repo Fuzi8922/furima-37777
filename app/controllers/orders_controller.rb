ac386a3bf8bb3bdd27cc133e8334ebc3a32ad010
Fuzi8922 <voyage1969to70@gmail.com>
2022-04-12 11:06:23 +0900

    ordersコントローラーのindexアクションを定義、ビューファイルを適切に表示できるようにプライベートメソッドを記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,13 @@
 class OrdersController < ApplicationController
+
+  def index
+    @item = Item.find(params[:item_id])
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit().merge(item_id: @item.id)
+  end
+
 end
