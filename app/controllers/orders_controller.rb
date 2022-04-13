5fb915a4666564432fbe86340254483145b376fc
Fuzi8922 <voyage1969to70@gmail.com>
2022-04-13 14:48:37 +0900

    ログアウト状態のユーザーおよび出品者本人は商品の購入ページには遷移できないよう定義

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,18 +1,28 @@
 class OrdersController < ApplicationController
 
+  before_action :authenticate_user!, only: [:index, :create]
+
   def index
     @item = Item.find(params[:item_id])
-    @order_address = OrderAddress.new
+    if @item.user_id != current_user.id
+      @order_address = OrderAddress.new
+    else
+      redirect_to items_path
+    end
   end
 
   def create
     @order_address = OrderAddress.new(order_params)
-    if @order_address.valid?
-      @order_address.save
-      return redirect_to root_path
+    if @order_address.user_id == current_user.id
+      if @order_address.valid?
+        @order_address.save
+        return redirect_to root_path
+      else
+        @item = Item.find(params[:item_id])
+        render :index
+      end
     else
-      @item = Item.find(params[:item_id])
-      render :index
+      redirect_to items_path
     end
   end
 
