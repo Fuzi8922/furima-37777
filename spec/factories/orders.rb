364b167d7b7f26d12f415d71b5c63f0ef3115cb6
Fuzi8922 <voyage1969to70@gmail.com>
2022-04-13 17:26:17 +0900

    orderモデル単体テストコードの実装

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -1,5 +1,15 @@
 FactoryBot.define do
-  factory :order do
-    
+
+  factory :order_address do
+    token            {"token_asdfghjkl0000000000"}
+    post_code        {"123-4567"}
+    prefecture_id    {1}
+    municipalities   {"city"}
+    address          {"1-1"}
+    building_address {"11"}
+    phone_number     {11122223333}
+    user_id          {1}
+    item_id          {1}
   end
+
 end
