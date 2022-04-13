4eae8bca46a5958c7d47c7d656750fd76a4d8ffe
Fuzi8922 <voyage1969to70@gmail.com>
2022-04-13 13:31:41 +0900

    バリデーションの設定

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,7 +1,18 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :post_code, :prefecture_id, :municipalities, :address, :building_address, :phone_number, :order, :user_id, :item_id
+  attr_accessor :post_code, :prefecture_id, :municipalities, :address, :building_address, :phone_number, :user_id, :item_id
   attr_accessor :token
+
+  with_options presence: true do
+    validates :token
+    validates :post_code,    format:{with: /\A\d{3}[-]\d{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)"}
+    validates :prefecture_id
+    validates :municipalities
+    validates :address
+    validates :phone_number, format:{with: /\A\d{10,11}\z/, message: "is invalid. Input only number"}
+    validates :user_id
+    validates :item_id
+  end
 
   def save
     order = Order.create(item_id: item_id, user_id: user_id)
