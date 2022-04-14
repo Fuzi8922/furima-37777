231f502296bb0b1abd4852bbe7c941fc3dc1f7d7
Fuzi8922 <voyage1969to70@gmail.com>
2022-04-14 11:08:42 +0900

    バリデーションの修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -6,7 +6,7 @@ class OrderAddress
   with_options presence: true do
     validates :token
     validates :post_code, format: { with: /\A\d{3}-\d{4}\z/, message: 'is invalid. Enter it as follows (e.g. 123-4567)' }
-    validates :prefecture_id
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :municipalities
     validates :address
     validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid. Input only number' }
